6f51fb9ab4854b147215a0abf53594e6e74af161
mittimi <mittings@gmail.com>
2024-09-22 15:29:44 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+@author: mittimi
+@title: mittimiLoadText
+@nickname: mittimiLoadText
+@description: This node can easily switch text or prompts by saving presets.
+"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+WEB_DIRECTORY = "./js"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
